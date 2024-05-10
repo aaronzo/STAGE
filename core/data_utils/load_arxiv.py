8b780d0664e28b2fc35e2641aaa1595d5049a303
Jack Boylan <boylan.jack@gmail.com>
2024-05-10 17:14:38 +0000

    Changes how arxiv data is loaded (torch geometric function error and pandas error)

--- core/data_utils/load_arxiv.py
+++ core/data_utils/load_arxiv.py
@@ -6,8 +6,11 @@ import pandas as pd
 
 def get_raw_text_arxiv(use_text=False, seed=0):
 
+    # dataset = PygNodePropPredDataset(
+    #     name='ogbn-arxiv', transform=T.ToSparseTensor())
+    # https://github.com/pytorch/pytorch/issues/111359#issuecomment-1818013629
     dataset = PygNodePropPredDataset(
-        name='ogbn-arxiv', transform=T.ToSparseTensor())
+        name='ogbn-arxiv', transform=T.Compose([T.ToUndirected(),T.ToSparseTensor()]))
     data = dataset[0]
 
     idx_splits = dataset.get_idx_split()
@@ -21,15 +24,24 @@ def get_raw_text_arxiv(use_text=False, seed=0):
     data.val_mask = val_mask
     data.test_mask = test_mask
 
-    data.edge_index = data.adj_t.to_symmetric()
+    # data.edge_index = data.adj_t.to_symmetric()
+    data.edge_index = data.adj_t
     if not use_text:
         return data, None
 
     nodeidx2paperid = pd.read_csv(
         'dataset/ogbn_arxiv/mapping/nodeidx2paperid.csv.gz', compression='gzip')
+    nodeidx2paperid['paper id'] = nodeidx2paperid['paper id'].astype(int)
 
     raw_text = pd.read_csv('dataset/ogbn_arxiv_orig/titleabs.tsv',
-                           sep='\t', header=None, names=['paper id', 'title', 'abs'])
+                        sep='\t', 
+                        # header=None, 
+                        skiprows=1,
+                        names=['paper id', 'title', 'abs'],
+    )
+    # import ipdb;ipdb.set_trace()
+    raw_text['paper id'] = raw_text['paper id'].astype(int, errors='ignore')
+    
     df = pd.merge(nodeidx2paperid, raw_text, on='paper id')
     text = []
     for ti, ab in zip(df['title'], df['abs']):
