4954f4ba30ab112284621c7f38f056add92e227a
Jack Boylan <boylan.jack@gmail.com>
2024-05-12 08:29:32 +0000

    Avoids OOM errors

--- core/LMs/generate_embeddings.py
+++ core/LMs/generate_embeddings.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn.functional as F
 import numpy as np
 
 from transformers import (
@@ -11,8 +12,18 @@ from tqdm import tqdm
 import argparse
 import os
 import logging
+import gc
 
-logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+logger = logging.getLogger(__name__)    
+
+def clear_cuda_cache():
+    torch.cuda.empty_cache()
+
+def free_memory(*args):
+    for arg in args:
+        del arg
 
 
 def parse_args():
@@ -38,11 +49,12 @@ def get_detailed_instruct(task_description: str, query: str) -> str:
 
 def generate_sfr_embedding_mistral(text, emb_path):
 
-    BATCH_SIZE = 128
-    max_length = 4096
-    num_nodes = len(text)
+    BATCH_SIZE = 64
+    max_length = 2048
+    EMBED_DIM = 4096
+    num_nodes = len(text)  # 169343
 
-    emb = np.memmap(emb_path, dtype=np.float16, mode='w+', shape=(num_nodes, max_length))
+    emb = np.memmap(emb_path, dtype=np.float16, mode='w+', shape=(num_nodes, EMBED_DIM))
 
     # load model and tokenizer
     # ref --> https://huggingface.co/Salesforce/SFR-Embedding-Mistral#transformers
@@ -52,6 +64,10 @@ def generate_sfr_embedding_mistral(text, emb_path):
         device_map='auto',
         torch_dtype=torch.float16
         )
+    
+    task_description = 'Identify the main and secondary category of Arxiv papers based on the titles and abstracts'
+    logger.info(f"Using instruction <<{task_description}>>")
+    text = [get_detailed_instruct(task_description, t) for t in text]
 
 
     for i in tqdm(range(0, len(text), BATCH_SIZE)):
@@ -67,9 +83,16 @@ def generate_sfr_embedding_mistral(text, emb_path):
         embeddings = last_token_pool(
             outputs.last_hidden_state, batch_dict['attention_mask']
         )
+        embeddings = F.normalize(embeddings, p=2, dim=1)
+        if i < BATCH_SIZE:
+            logger.info(f"embeddings shape: {embeddings.shape}")
 
         # Save embeddings to memmap
         emb[i:i+BATCH_SIZE] = embeddings.detach().cpu().numpy().astype(np.float16)
+
+        free_memory(embeddings, outputs, batch_dict)
+        clear_cuda_cache()
+        gc.collect()
 
     emb.flush()
     return
@@ -79,9 +102,9 @@ def generate_sfr_embedding_mistral(text, emb_path):
 def generate_embeddings_and_save(args):
 
     emb_dir = f"prt_lm/{args.dataset_name}"
-    os.makedirs(emb_dir, exist_ok=True)
-
     emb_path = f"{emb_dir}/{args.lm_model_name}-seed{args.seed}.emb"
+
+    os.makedirs('/'.join(emb_path.split('/')[:-1]), exist_ok=True)
 
     logger.info(f"Generating embeddings for {args.dataset_name} using model: {args.lm_model_name}")
     logger.info(f"EMbeddings will be saved to {emb_path}")
