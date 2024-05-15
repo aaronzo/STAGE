3dbd2419b76ed5d89ceb075f74fb26f58e545358
Jack Boylan <boylan.jack@gmail.com>
2024-05-15 10:24:37 +0000

    Modifies hyperparam search pattern

--- hyperparam_search.sh
+++ hyperparam_search.sh
@@ -1,14 +1,15 @@
-for dataset in 'ogbn-products' 'cora' 'pubmed' 'arxiv_2023' 'ogbn-arxiv'
+for dropout in 0.1 0.3 0.5
 do
-    for lm_model in 'Alibaba-NLP/gte-Qwen1.5-7B-instruct' 'Salesforce/SFR-Embedding-Mistral'
+    for hidden_dim in 128 256 512
     do
         for num_layers in 4 6 8
         do
-            for hidden_dim in 128 256 512
+            for dataset in 'ogbn-arxiv' 'ogbn-products' 'cora' 'pubmed' 'arxiv_2023'
             do
-                for dropout in 0.1 0.3 0.5
+                for lm_model in 'Salesforce/SFR-Embedding-Mistral' 'Alibaba-NLP/gte-Qwen1.5-7B-instruct'
                 do
                     python -m core.trainEnsemble gnn.train.feature_type TA_P dataset ${dataset} seed 42 lm.model.name ${lm_model} gnn.model.num_layers ${num_layers} gnn.model.hidden_dim ${hidden_dim} gnn.train.dropout ${dropout} #>> ${dataset}_${lm_model}_layers${num_layers}_dim${hidden_dim}_dropout${dropout}.out
+                    echo "######## Finised experiment with ${dataset}_${lm_model}_layers${num_layers}_dim${hidden_dim}_dropout${dropout} ###########"
                 done
             done
         done
